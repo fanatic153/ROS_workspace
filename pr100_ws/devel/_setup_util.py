c03e9e185958756d80675f3e4cfe7bf2f4077228
fanatic153 <fanatic153153@gmail.com>
2021-06-03 15:41:09 +0800

    Fixed room.launch in pr100_ws

--- pr100_ws/devel/_setup_util.py
+++ pr100_ws/devel/_setup_util.py
@@ -268,7 +268,7 @@ if __name__ == '__main__':
 
         if not args.local:
             # environment at generation time
-            CMAKE_PREFIX_PATH = r'/home/janet/ROS_workspace/ddrobot_ws/devel;/home/janet/ROS_workspace/turtlebot_ws/devel;/opt/ros/noetic'.split(';')
+            CMAKE_PREFIX_PATH = r'/home/janet/ROS_workspace/pr100_ws/devel;/home/janet/ROS_workspace/ddrobot_ws/devel;/home/janet/ROS_workspace/turtlebot_ws/devel;/opt/ros/noetic'.split(';')
         else:
             # don't consider any other prefix path than this one
             CMAKE_PREFIX_PATH = []
